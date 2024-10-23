85471bd5df507108dc3fc66f8dda8a58ce1febe7
kravenvijay04 <kravenvijay04@users.noreply.github.com>
2024-10-23 22:28:28 +0530

    post for login and notes is added

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { authenticateToken } = require("./utilities");
 const User = require("./models/user.model");
+const Note = require("./models/notes.model");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -22,6 +23,7 @@ app.get("/", (req, res) => {
     res.json({ data: "hello" });
 });
 
+//Create an Account
 app.post("/create-account", async (req, res) => {
     try {
         const { fullName, email, password } = req.body;
@@ -67,6 +69,81 @@ app.post("/create-account", async (req, res) => {
         return res.status(500).json({ error: true, message: "Internal Server Error" });
     }
 });
+
+
+//login an Account
+app.post("/login",async(req,res)=>{
+    const{email,password}=req.body;
+
+    if(!email){
+        return res.status(400).json({message:"Email is required"});
+
+    }
+    if(!password){
+        return res.status(400).json({message:"password is required"});
+
+    }
+    const userInfo = await User.findOne({email:email});
+    if(!userInfo){
+        return res.status(400).json({message:"User not found"});
+    }
+    if(userInfo.email == email && userInfo.password==password ){
+        const user = {user:userInfo};
+        const accessToken= jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:"36000m",
+        });
+
+        return res.json({
+            error:false,
+            message:"Login Succesfully",
+            email,
+            accessToken,
+        });
+    }
+    else{
+        return res.status(400).json({
+            error:true,
+            message:"Invalid Credentials",
+        })
+    }
+});
+
+//Add Note
+app.post("/add-notes",authenticateToken,async(req,res)=>{
+    const {title,content,}=req.body;
+    const {user} = req.user;
+
+    if(!title){
+        return res.status(400).json({error:true,message:"Title is required"});
+    }
+
+    if(!content){
+        return res.status(400).json({error:true,message:"Content is required"});
+    }
+
+    try{
+        const note = new Note({
+            title,
+            content,
+            userId:user._id
+        })
+
+        await note.save();
+
+        return res.json({
+            error:false,
+            note,
+            message:"Note added successfully",
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            error:true,
+            message:'Internet server Error',
+        });
+    }
+
+})
 
 // Start the server
 app.listen(PORT, () => {
