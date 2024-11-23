cf5880de69a4bed17b8273e7a5eaee0c0282e3ec
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-11-23 21:33:34 +0530

    some change

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import './Navbar.css';
 import { useState } from 'react';
-import ProfileInfo from '../cards/profileInfo'; 
+import ProfileInfo from '../cards/profileInfo';
 import { useNavigate } from 'react-router-dom';
 import SearchBar from '../Searchbar/searchBar';
 
@@ -11,10 +11,10 @@ const Navbar = () => {
     const navigate = useNavigate(); // Call useNavigate as a function
 
     const onlogout = () => {
-      console.log("Logout clicked"); // Add this line to verify if it's triggered
-      navigate("/login");
-  };
-  
+        console.log("Logout clicked"); // Add this line to verify if it's triggered
+        navigate("/login");
+    };
+
 
     const handleSearch = () => {
         // Implement your search logic here
@@ -26,11 +26,11 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            <h1>Notify</h1>
-            <SearchBar 
-                value={searchQuery} 
+            <h1>Notify.v1</h1>
+            <SearchBar
+                value={searchQuery}
                 onChange={({ target }) => setSearchQuery(target.value)}
-                handleSearch={handleSearch} 
+                handleSearch={handleSearch}
                 onClearSearch={onClearSearch}
             />
             <ProfileInfo onlogout={onlogout} /> {/* Pass the onlogout function as a prop */}
