fbbb18374ee028659e7c966d3b8873e5dc16753f
kravenvijay04 <123456789+odin@users.noreply.github.com>
2024-12-14 16:54:36 +0530

    some changes

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -4,17 +4,21 @@ import Navbar from '../../components/Navbar/Navbar';
 import { Link } from "react-router-dom";
 import Password from "../../components/input/Password.jsx";
 import { validateEmail } from '../../utils/helper.js';
+import { useNavigate } from "react-router-dom";
 import "./signUp.css";
+import axiosInstance from '../../utils/axiosInstance.js';
 
 const SignUp = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [error, setError] = useState(null);
-  
+
+  const navigate = useNavigate()
+
   const handleSignup = async (e) => {
     e.preventDefault();
-    
+
     if (!name) {
       setError("Please enter your name");
       return;
@@ -30,6 +34,33 @@ const SignUp = () => {
 
     setError("");
 
+    try {
+      const response = await axiosInstance.post("/create-account", {
+        fullName: name,
+        email: email,
+        password: password
+      })
+
+      if (response.data && response.data.error) {
+        setError(response.data.message);
+        return
+      }
+
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        navigate('/dashboard');
+      }
+
+    }
+    catch (error) {
+      if (error.response && error.response.data && error.response.data.message) {
+        setError(error.response.data.message);
+      }
+      else {
+        setError("An unexpected error occured. Please try again");
+      }
+    }
+
   }
 
   return (
@@ -38,28 +69,28 @@ const SignUp = () => {
       <div id="Signup-box">
         <form onSubmit={handleSignup}>
           <h3>Sign Up</h3>
-          <input 
-            type='text' 
-            placeholder='Name' 
-            className='input' 
+          <input
+            type='text'
+            placeholder='Name'
+            className='input'
             value={name}
-            onChange={(e) => setName(e.target.value)} 
+            onChange={(e) => setName(e.target.value)}
           />
-          <input 
-            type='text' 
-            placeholder='Email' 
-            className='input' 
+          <input
+            type='text'
+            placeholder='Email'
+            className='input'
             value={email}
-            onChange={(e) => setEmail(e.target.value)} 
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <Password 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
+          <Password
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           {error && <p className='info'>{error}</p>}
           <button type='submit' className='Signup-btn'>Sign Up</button>
           <p className='Notreg'>
-            Already have an Account?{" "} 
+            Already have an Account?{" "}
             <Link to="/login">Login</Link>
           </p>
         </form>
