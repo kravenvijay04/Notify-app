d4064d7c8eb28628cfd3dec7fa8baacac62541bc
kravenvijay04 <vijaykumarnr04@gmail.com>
2024-12-22 19:02:30 +0530

    enhanced the pop-up feature

--- frontend/src/components/cards/profileInfo.jsx
+++ frontend/src/components/cards/profileInfo.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import "./profileInfo.css";
 import { getInitial } from "../../utils/helper";
@@ -6,30 +5,31 @@ import { getInitial } from "../../utils/helper";
 const ProfileInfo = ({ userInfo, onlogout }) => {
   const [isPopupVisible, setPopupVisible] = useState(false);
 
-  const handleMouseEnter = () => {
-    setPopupVisible(true);
-  };
-
-  const handleMouseLeave = () => {
-    setPopupVisible(false);
+  const handleTogglePopup = () => {
+    setPopupVisible((prev) => !prev);
   };
 
   return (
     <>
       {isPopupVisible && (
-        <div id="pop-info"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}>
+        <div id="pop-info">
           <p>Username: {userInfo?.fullname}</p>
-          <p>Email: <span id="email">{userInfo?.email}</span></p>
+          <p>
+            <span id="email">{userInfo?.email}</span>
+          </p>
+          <button id="logout-btn1" onClick={onlogout}>
+            Logout
+          </button>
         </div>
       )}
+      <div id="b"></div>
       <div id="box-profile">
-        <div id="nameID"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}>
-          {getInitial(userInfo?.fullname || "Guest")}</div>
-        <button id="logout-btn" onClick={onlogout}>Logout</button>
+        <div id="nameID" onClick={handleTogglePopup}>
+          {getInitial(userInfo?.fullname || "Guest")}
+        </div>
+        <button id="logout-btn" onClick={onlogout}>
+          Logout
+        </button>
       </div>
     </>
   );
