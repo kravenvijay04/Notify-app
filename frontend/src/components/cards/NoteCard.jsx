3800614b2a5c3df67d28a4456f19956055bc6631
kravenvijay04 <vijaykumarnr04@gmail.com>
2024-12-23 22:54:43 +0530

    some changes in jsx code

--- frontend/src/components/cards/NoteCard.jsx
+++ frontend/src/components/cards/NoteCard.jsx
@@ -13,12 +13,14 @@ const NoteCard = ({ title, content, date, isPinned, onEdit, onDelete, onPinNote 
           <h3 className='title'>{title}</h3>
           <span className='date'>{moment(date).format('Do MMM YYYY')}</span>
         </div>
-        <MdOutlinePushPin 
-  className={`icon-btn ${isPinned ? 'text-primary' : 'text-slate-300'}`} 
-  onClick={onPinNote} 
-/>
+        <MdOutlinePushPin
+          className={`icon-btn ${isPinned ? 'text-primary' : 'text-slate-300'}`}
+          onClick={onPinNote}
+        />
       </div>
-      <p className='content'>{content}</p>
+      <p className="content">{content.length > 39 ? `${content.substring(0, 35)}...` : content}</p>
+
+
       <div id='ons'>
         <MdCreate className='onCreate' onClick={onEdit} />
         <MdDelete className='onDelete' onClick={onDelete} />
