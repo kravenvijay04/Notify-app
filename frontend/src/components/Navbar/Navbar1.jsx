f1651bee3b341509219be52605db8e8f9b4d1c6f
kravenvijay04 <vijaykumarnr04@gmail.com>
2024-12-28 14:08:28 +0530

    changes for responsive styles

---
+++ frontend/src/components/Navbar/Navbar1.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Navbar1.css';
+
+
+const Navbar1 = () => {
+
+
+    return (
+        <div className="navbar1">
+            <div className='title'>
+                <img src="./images/logo.png" alt="notify logo" className='logo' />
+                <h1>Notify</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar1;
