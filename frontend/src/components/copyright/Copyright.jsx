59526c193580c869481b32caacc4a221d83d4d90
kravenvijay04 <vijaykumarnr04@gmail.com>
2025-01-01 23:15:21 +0530

    Copyright file is added

---
+++ frontend/src/components/copyright/Copyright.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Copyright.css'
+const Copyright = () => {
+  return (
+    <div className='copy'>
+    <img src="./images/rounded.png" alt="notify logo" className='rounded' />
+      <div id='upper'>
+        <p>about</p>
+        <p>help</p>
+        <p>contact</p>
+      </div>
+    </div>
+  )
+}
+
+export default Copyright
